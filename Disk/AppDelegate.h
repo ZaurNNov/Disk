7ee14d5f14447f955d11ab564c331600559c35aa
Zaur Giyasov <zagiyas@mera.ru>
2018-08-22 12:38:18 +0300

    Initial Commit

---
+++ Disk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Disk
+//
+//  Created by Zaur Giyasov on 22/08/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Disk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Disk
+//
+//  Created by Zaur Giyasov on 22/08/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
