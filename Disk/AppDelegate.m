3cfb6fed8defba8a1b0d6f5871d0c50ac62a9a08
Zaur Giyasov <zagiyas@mera.ru>
2018-08-22 15:26:32 +0300

    url schema not working Callback URL: yaddisk://com.rus.nnov.sovetsquare.home.Disk

--- Disk/AppDelegate.m
+++ Disk/AppDelegate.m
@@ -7,8 +7,11 @@
 //
 
 #import "AppDelegate.h"
+#import <YandexLoginSDK/YandexLoginSDK.h>
 
 @interface AppDelegate ()
+
+@property (nonatomic) NSString *clientID;
 
 @end
 
@@ -17,35 +20,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.clientID = [NSString stringWithFormat:@"4fa610e1087e4cacb46f15f2c2f9d713"];
+    
+    NSError *error = nil;
+    @try {
+        
+        [YXLSdk.shared activateWithAppId: self.clientID error:&error];
+    } @catch (NSException *exception) {
+        NSLog(@"** Exception %@", exception.debugDescription);
+    } @finally {
+        //
+    }
+    
     return YES;
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+-(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    return [YXLSdk.shared handleOpenURL:url sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
 }
 
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 
 @end
--- Disk/ViewController.m
+++ Disk/ViewController.m
@@ -7,23 +7,110 @@
 //
 
 #import "ViewController.h"
+#import <YandexLoginSDK/YandexLoginSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <YXLObserver, YXLLoginResult>
 
 @end
 
 @implementation ViewController
 
+@synthesize jwt;
+@synthesize token;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // clear all tokens
+    [YXLSdk.shared logout];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
 }
 
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self addMeAsObserver];
+    [self autorizeYAD];
+    
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+    
+    NSLog(@"%@", self); // check point
+}
+
+-(void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [self removeMeFromObservers];
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+-(void)autorizeYAD {
+    [YXLSdk.shared authorize];
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+// Add observers
+-(void)addMeAsObserver {
+    [YXLSdk.shared addObserver:self];
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+-(void)removeMeFromObservers {
+    [YXLSdk.shared removeObserver:self];
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+// Delegate methods
+- (void)loginDidFinishWithError:(nonnull NSError *)error {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)loginDidFinishWithResult:(nonnull id<YXLLoginResult>)result {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)setNeedsFocusUpdate {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
+
+- (void)updateFocusIfNeeded {
+    NSLog(@"Log: %@: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+}
 
 @end
+
